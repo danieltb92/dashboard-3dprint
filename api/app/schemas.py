dba82988de5783afd7cd0fb20d15737dabd50911
Daniel Tunjano <danieltb92@hotmail.com>
2026-09-07 17:18:49 -0500

    feat: implement pydantic schemas

---
+++ api/app/schemas.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+
+class PrinterBase(BaseModel):
+    name: str
+
+
+class MaterialBase(BaseModel):
+    name: str
+    cost_per_gram: float
+
+
+class ProductBase(BaseModel):
+    name: str
+    material_id: int
+    base_cost: float
+
+
+class ProductionLotBase(BaseModel):
+    product_id: int
+    quantity: int
+
+
+class SaleBase(BaseModel):
+    product_id: int
+    quantity: int
+    price: float
+
+
+class QuoteBase(BaseModel):
+    client_id: int
+    product_id: int
+    quantity: int
+
+
+class ClientBase(BaseModel):
+    name: str
+
+
+class SettingBase(BaseModel):
+    key: str
+    value: str
